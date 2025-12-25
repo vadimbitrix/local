c99cf8c056255d57f5f97ff33ad52790b4e8fe10
devob <info@devob.ru>
2025-12-25 18:00:29 +0300

    added html for notification

---
+++ include/system/popup.php
@@ -0,0 +1,24 @@
+<div class="cookie-notification cookie-notification--hidden" id="cookieNotification">
+    <div class="cookie-notification__container">
+        <button class="cookie-notification__close" id="cookieClose">
+            <span class="cookie-notification__close-icon">×</span>
+        </button>
+
+        <div class="cookie-notification__content">
+            <p class="cookie-notification__text">
+                Продолжая просмотр данного сайта, Вы соглашаетесь с обработкой персональной информации, в соответствии с <a class="cookie-notification__link" href="/conf/">политикой в отношении обработки персональных данных сайта</a>.
+            </p>
+            <p class="cookie-notification__text">
+                На сайте осуществляется сбор персональных данных посредством метрических программ, в том числе с использованием файлов cookie.
+            </p>
+            <div class="cookie-notification__actions">
+                <button class="cookie-notification__button cookie-notification__button--secondary" id="cookieLearnMore">
+                    Узнать подробнее
+                </button>
+                <button class="cookie-notification__button cookie-notification__button--primary" id="cookieAccept">
+                    Принять
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
